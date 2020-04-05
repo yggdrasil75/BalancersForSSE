e67092ffc50ea7e1085a64872ab0ebaec864e651
yggdrasil75 <cblackburn7557@gmail.com>
2020-04-05 13:54:39 -0400

    began crafting logic

---
+++ CraftingLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class CraftingLogic
+{
+	public CraftingLogic()
+	{
+		
+	}
+	private GenerateFromTrusted()
+	{
+
+	}
+	private conditions()
+	{
+		//check if ccor is installed
+	}
+	private ConditionsCCOR()
+	{
+
+	}
+	private Perks()
+	{
+
+	}
+	private WeapCraft()
+	{
+
+	}
+	private ArmoCraft()
+	{
+
+	}
+	private AmmoCraft()
+	{
+
+	}
+
+}
