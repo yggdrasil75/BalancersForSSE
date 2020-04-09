3975e66af03254b451b3728ef4227afd08915e01
yggdrasil75 <cblackburn7557@gmail.com>
2020-04-09 17:24:33 -0400

    Update YggJsonSerialize.pas

--- YggJsonSerialize.pas
+++ YggJsonSerialize.pas
@@ -1,11 +1,11 @@
 unit YggJsonSerialize;
 
 const
-	MainDir := DataPath + '/tools/YggScript/Jsons/';
+	MainDir = DataPath + '/tools/YggScript/Jsons/';
 	
-function initialize;
+function initialize: integer;
 var
-	i,j:integer;
+	i,j, eccf:integer;
 	cf: IInterface;
 	cfDir:string;
 	Header: TJsonObject;
@@ -14,18 +14,19 @@ begin
 		Cf := FileByIndex(i);
 		BeginUpdate(cf);
 		try
-			eccf := ElementCount(cf)
-			cfDir := forcedir(MainDir + '\' + FileName(cf) + '\');
+			eccf := ElementCount(cf);
+			cfDir := ForceDirectories(MainDir + '\' + GetFileName(cf) + '\');
 			Header := RecordToJson(ElementByIndex(cf, 0));
-			if FileExists(cfDir + 'header.json') then begin
-				
-				//if crc32 of header matches crc32 of plugin then skip plugin\
+			HeaderJson := cfDir + 'header.json';
+			if FileExists(HeaderJson) then begin
+				Header := LoadFromFile(HeaderJson, true, nil);
+				if GetString('CRC') = wbCRC32File(GetFileName(cf)) then continue;
 				//unless the script has a major version number change
 			end else begin
 				//create the file
 				tempList := TStringlist.create;
 				TempList.add(Obj.ToJSON(false));
-				tempList.SaveToFile(cfDir + 'header.json');
+				tempList.SaveToFile(HeaderJson);
 			end;
 			if eccf > 0 then begin
 				for j := elementcount(cf) - 1 downto 1 do begin
@@ -70,12 +71,12 @@ begin
 			CE := ElementByIndex(CR, i);
 			cp := path(CE);
 			while ContainsText(cp, ' \ ') do begin
-				cpi := POS(' \ ', Path(CP));
-				cp := copy(cp, cpi + 4, lenght(cp));
+				cpi := pos(' \ ', CP);
+				cp := copy(cp, cpi + 4, length(cp));
 			end;
 			if assigned(ce) then begin
 				if ElementCount(ce) <> 0 then
-					CrJson.A[cp].Add(RecordToJson(ce));
+					CrJson.A[cp].Add(RecordToJson(ce))
 				else 
 					crJson.S[cp] := GetEditValue(CE);
 			end;
@@ -85,3 +86,5 @@ begin
 		CRJson.Free;
 	end;
 end;
+
+end.
